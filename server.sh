5788f13173d60cb3599238ac06773d0bfe950dd7
Aefer <ziyuei2005@gmail.com>
2020-01-04 20:30:02 +0800

    Update

--- server.sh
+++ server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SERVICE='paperclip.jar'
 SCREENNAME='minecraft_server'
-USERNAME='user'
+USERNAME='runner'
 WORLD='world'
-MCPATH='/home/user/MCGA'
+MCPATH='/home/runner/work/MCGA/MCGA'
 #MCPATH='/home/runner/work/MCGA/MCGA'
 BACKUPPATH='/MCGA/minecraft.backup'
-MAXHEAP=2048
-MINHEAP=2048
+MAXHEAP=6144
+MINHEAP=6144
 HISTORY=1024
 CPU_COUNT=2
 INVOCATION="java -Xmx${MAXHEAP}M -Xms${MINHEAP}M -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UseLargePagesInMetaspace -Dusing.aikars.flags=mcflags.emc.gs -jar ${SERVICE}" 
